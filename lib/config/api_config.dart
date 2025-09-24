b3132a22b88b3eb3075ef2d96cd745ca94de460a
FAYH <94425967+younot34@users.noreply.github.com>
2025-09-24 10:13:10 +0700

    Update api_config.dart

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = "http://localhost:8000/api";
+  static const String baseUrl = "https://a-meet.cloud/api";
   // emulator pakai 10.0.2.2, kalau device fisik ganti IP laptop
   static const Map<String, String> headers = {
     "Content-Type": "application/json",
